c4075100c02eb82093b702a726ce4adfa0ce333d
rudikurniawan99 <rudikurniawan501@gmail.com>
2021-12-13 10:39:49 +0700

    minor update

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,12 +1,14 @@
 import NextAuth from "next-auth/next";
 import SpotifyProvider from 'next-auth/providers/spotify'
+import { LOGIN_URL } from "../../../lib/spotify";
 
 export default NextAuth({
   providers: [
     SpotifyProvider({
       clientId: process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID,
       clientSecret: process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_SECRET,
-      authorization
+      authorization: LOGIN_URL
     })
-  ]
+  ],
+  secret: process.env.JWT_SECRET
 })
