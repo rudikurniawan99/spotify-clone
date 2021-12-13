f1d1458ff6bc90acb641a0778bb39ebbd924deec
rudikurniawan99 <rudikurniawan501@gmail.com>
2021-12-13 13:55:02 +0700

    add callback and refreshAccessToken function

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,6 +1,6 @@
 import NextAuth from "next-auth/next";
 import SpotifyProvider from 'next-auth/providers/spotify'
-import { LOGIN_URL } from "../../../lib/spotify";
+import spotifyApi, { LOGIN_URL } from "../../../lib/spotify";
 
 export default NextAuth({
   providers: [
@@ -10,5 +10,43 @@ export default NextAuth({
       authorization: LOGIN_URL
     })
   ],
-  secret: process.env.JWT_SECRET
+  secret: process.env.JWT_SECRET,
+  pages: {
+    signIn: '/auth/login'
+  },
+  callbacks: {
+    async jwt({ token, user, account }){
+      if(account && user){
+        return {
+          ...token,
+          accessToken: account.access_token,
+          refreshToken: account.refresh_token,
+          username: account.providerAccountId,
+          accessTokenExpires: account.expires_at * 1000
+        }
+      }
+      if(Date.now() < account.accessTokenExpires){
+        return token
+      }
+
+      return await refreshAccessToken(token)
+    }
+  }
+
 })
+
+const refreshAccessToken = async (token) => {
+  try {
+    spotifyApi.setAccessToken(token.accessToken)
+    spotifyApi.setRefreshToken(token.refreshToken)
+
+    const { body: refreshedToken } = await spotifyApi.refreshAccessToken
+
+  } catch (e) {
+    console.log(e) 
+    return {
+      ...token,
+      error: 'Refresh Token Error'
+    }
+  }
+}
