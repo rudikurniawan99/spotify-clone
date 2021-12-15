b70d1e30c190be2208b6402b918b8d600ad9bae1
rudikurniawan99 <rudikurniawan501@gmail.com>
2021-12-15 16:40:47 +0700

    create middleware

---
+++ pages/_middleware.js
@@ -0,0 +1,21 @@
+import { getToken } from "next-auth/jwt";
+import { NextResponse } from "next/server";
+
+export async function middleware(req){
+  const token = await getToken({
+    req,
+    secret: process.env.JWT_SECRET
+  })
+
+  const { pathname } = req.nextUrl
+  console.log(pathname)
+
+  // allow request if the following true
+  if(pathname.includes('/api/auth') || token){
+    return NextResponse.next()
+  }
+
+  if(!token && pathname !== '/login'){
+    return NextResponse.redirect('/login')
+  }
+}
